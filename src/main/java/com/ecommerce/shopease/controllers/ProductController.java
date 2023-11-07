0d6cc7c2ca76a06e9662c5962cad81a9db5eb1ff
MihaiTirnaveanu <mihaitirnaveanu@gmail.com>
2023-11-07 21:49:45 +0200

    create api for products connect with the psql database

---
+++ src/main/java/com/ecommerce/shopease/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.ecommerce.shopease.controllers;
+
+import com.ecommerce.shopease.models.Product;
+import com.ecommerce.shopease.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
